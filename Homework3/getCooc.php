6f29d5cd870191897e0df3a76c77a216d5813ffe
Wake Wake <Wake Wake>
2016-06-16 14:13:46 +0800

    bug fixed orz

--- Homework3/getCooc.php
+++ Homework3/getCooc.php
@@ -21,7 +21,6 @@ if ($word1 != null || $word2 != null)
         $request = $word1 . "_*";
     else if ($word2 != null && $word1 == null) 
         $request = $word2 . "_*";
-    $curl = ($url) . $request;
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $curl);
@@ -31,29 +30,30 @@ if ($word1 != null || $word2 != null)
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $curl_result = curl_exec($ch);
     curl_close($ch);
-    // echo $curl_result;
     
-    echo "<!DOCTYPE HTML>";
-    echo "<head>";
-    echo "<meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\">";
-    echo "</head>";
-    echo "<body>";
-    echo "<table border=\"1\"><thead><tr><th>Word</th><th>Count</th></tr></thead><tbody>";
     $result = json_decode($curl_result, true);
+    echo "<table border=\"1\">";
+    echo "  <thead>";
+    echo "    <tr>";
+    echo "      <th>Word</th>";
+    echo "      <th>Count</th>";
+    echo "    </tr>";
+    echo "  </thead>";
+    echo "  <tbody>";
     for ($x = 0; $x < count($result['Row']); $x++) {
-        $row = $result['Row'][x];
-        $row['key'] = base64_decode($row['key']);
-        $len = count($row['Cell']);
-        $row['Cell'][0]['$'] = base64_decode($row['Cell'][0]['$']);
-        echo "<tr>";
-        echo "<td>" + $row['key'] + "</td>";
-        echo "<td>" + $row['Cell'][0]['$'] + "</td>";
-        echo "</tr>";
+        $row = $result['Row'][$x];
+        echo $x;
+        $key = base64_decode($row['key']);
+        $value = base64_decode($row['Cell'][0]['$']);
+        echo "    <tr>";
+        echo "      <td>" . $key . "</td>";
+        echo "      <td>" . $value . "</td>";
+        echo "    </tr>";
     }
-    echo "</tbody></table>";
-    echo "</body>";
-    exit;
+    echo "  </tbody>";
+    echo "</table>";
     // echo json_encode($result);
+    exit;
 }
     echo "<!DOCTYPE HTML>";
     echo "<head>";
@@ -64,7 +64,7 @@ if ($word1 != null || $word2 != null)
     echo "        <table>";
     echo "            <tr>";
     echo "                <td><label>Word1</label></td>";
-    echo "                <td><input type=\"text\" name=\"word1\" value=\"POST\" /></td>";
+    echo "                <td><input type=\"text\" name=\"word1\" /></td>";
     echo "            </tr>";
     echo "            <tr>";
     echo "                <td><label>Word2</label></td>";
